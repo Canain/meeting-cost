f939206ff78d4adbf3b0ff498f7b8870fcb91e0d
Shuyang Chen <shuyang_chen@intuit.com>
2016-06-20 13:41:31 -0700

    Changed to use numeral for number formatting

--- src/browser/ts/main.ts
+++ src/browser/ts/main.ts
@@ -12,23 +12,24 @@ import { Component, enableProdMode } from '@angular/core';
 import { MdInput } from '@angular2-material/input';
 import { MdSlideToggle } from '@angular2-material/slide-toggle';
 import { MdButton } from '@angular2-material/button';
+import numeral = require('numeral');
 
 @Component({
 	selector: 'meeting-cost',
 	template: `
-		<h1>{{formatted}}</h1>
-		<p>Elapsed Time: {{(diff / 1000).toFixed(0)}} seconds</p>
+		<h1>{{numeral(millisecondly * elapsed).format('$0,0.00')}}</h1>
+		<p>Elapsed Time: {{numeral(elapsed / 1000).format('00:00:00')}}</p>
 		<div>
 			<button md-raised-button (click)="start()">Start</button>
 			<button md-raised-button (click)="stop()">Stop</button>
 		</div>
 		<form>
 			<md-input [(ngModel)]="salary" placeholder="Average Yearly Salary">
-				<span md-prefix>$&nbsp;</span>
+				<span md-prefix>$</span>
 			</md-input>
 			<md-input [(ngModel)]="people" placeholder="People in Meeting"></md-input>
 		</form>
-		<p>Hourly Usage: {{money.format(hourly)}}</p>
+		<p>{{numeral(hourly).format('$0,0.00')}}/hour</p>
 	`,
 	directives: [ MdInput, MdSlideToggle, MdButton ],
 	styles: [`
@@ -45,38 +46,44 @@ export class MeetingCost {
 	days = 260;
 	hours = 8;
 	
-	formatted: string;
-	hourly: number;
-	diff: number;
+	hourly = 0;
+	millisecondly = 0;
+	elapsed = 0;
 
-	salary = 100000;
+	private _salary: number;
+
 	people = 10;
 
 	time = -1;
 
-	money = new Intl.NumberFormat('en-US', {
-		style: 'currency',
-		currency: 'USD',
-		minimumFractionDigits: 2
-	});
+	numeral = numeral;
 
 	constructor() {
+		window['app'] = this;
+
+		this.salary = 100000;
+
 		setInterval(this.update.bind(this), 50);
 		this.update();
 	}
 
 	update() {
-		this.diff = this.time < 0 ? 0 : performance.now() - this.time;
+		this.elapsed = this.time < 0 ? 0 : Date.now() - this.time;
+	}
 
-		this.hourly = this.salary * this.people / this.days / this.hours;
+	get salary() {
+		return this._salary;
+	}
 
-		const milisecondly = this.hourly / 60 / 60 / 1000;
+	set salary(value: number) {
+		this._salary = value;
 
-		this.formatted = this.money.format(this.diff * milisecondly);
+		this.hourly = this._salary * this.people / this.days / this.hours;
+		this.millisecondly =  this.hourly / 60 / 60 / 1000;
 	}
 
 	start() {
-		this.time = performance.now();
+		this.time = Date.now();
 	}
 
 	stop() {
