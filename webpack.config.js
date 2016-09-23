57f94697f260a79a1992d89d5886d09dfb78e252
Shuyang Chen <sc1085@gmail.com>
2016-09-22 22:20:35 -0400

    Built

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const webpack = require('webpack');
-
-const config = require('./webpack.common.js');
-
-config.watch = true,
-
-module.exports = config;
--- webpack.prod.config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const webpack = require('webpack');
-
-const config = require('./webpack.common.js');
-
-module.exports = config;
